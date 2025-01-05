951216a2d005b05cba9993a1389d0279f7d81e29
Ken69420 <dnielmranazam@gmail.com>
2025-01-05 11:39:08 +0800

    update the dockerization to also include nginx

--- api/server.js
+++ api/server.js
@@ -19,8 +19,14 @@ const PORT = process.env.PORT || 3000;
 //MongoDB Atlas URL
 const mongoURL = process.env.MONGO_URL;
 
+const corsOptions = {
+  origin: "https://stealthify.dewn.org",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
 //Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
